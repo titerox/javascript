5ece6de80b763726ee63f0fa72b06a6907f4b575
titerox <tite.182@hotmail.com>
2023-01-19 09:15:29 -0300

    condicoes

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 3
+
+
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
