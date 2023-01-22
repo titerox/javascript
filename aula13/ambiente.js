e5cba4881daa303d3af3b6063a9c6408390296f6
titerox <tite.182@hotmail.com>
2023-01-22 11:44:17 -0300

    while

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+
+/*
+while (c < 63232){
+    console.log(`Pass ${c}`)
+    c++
+}
+*/
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
+
+
+do{
+    console.log(`Tudo bem ${c}`)
+    c++0
+} while(c<6)
---
+++ aula13/node_4c5a314fc0ff3.tmp.js
@@ -0,0 +1,27 @@
+var c = 1
+
+/*
+while (c < 63232){
+    console.log(`Pass ${c}`)
+    c++
+}
+*/
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
+
+
+do{
+    console.log(`Tudo bem ${c}`)
+} while(c<6)
